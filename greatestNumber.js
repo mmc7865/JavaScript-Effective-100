9d778b5a347b8f068c06c3d95a7afc5979146fad
mmc7865 <mmc8791@gmail.com>
2024-10-11 22:45:15 +0530

    12th Program Done:

---
+++ greatestNumber.js
@@ -0,0 +1,17 @@
+// Accept two numbers and print the greatest between them
+
+let a = +prompt("Enter the first number")
+let b = +prompt("Enter the second number")
+
+if (a > b) {
+    console.log(`${a} is greatest`);
+    
+} 
+else if ( a == b){
+    console.log("Both numbers equal");
+    
+}else{
+    console.log(`${b} is greatest`);
+    
+
+}
