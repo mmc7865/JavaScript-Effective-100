e10a83e1fe88c28ce7ed986c07babad5055d67eb
mmc7865 <mmc8791@gmail.com>
2024-10-11 15:07:44 +0530

    3rd Program Done:

---
+++ acceptValueSum.js
@@ -0,0 +1,6 @@
+// Accept two integers from user and print the sum
+
+let num1 = +prompt("Enter your first Number")
+let num2 = +prompt("Enter your second Number")
+console.log(`The sum of ${num1} and ${num2} is = `, num1 + num2);
+
