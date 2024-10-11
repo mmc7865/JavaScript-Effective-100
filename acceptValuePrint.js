6abf3d6ebac4691fff3b057559bae30dd22ea584
mmc7865 <mmc8791@gmail.com>
2024-10-11 15:13:38 +0530

    4th program Done:

---
+++ acceptValuePrint.js
@@ -0,0 +1,6 @@
+// Accept the User's name, age and print in following manner
+// 	Ex - Hello Shery, you are 12 years old.
+
+let name = prompt("Enter Your Full Name");
+let age = prompt("Enter Your age");
+console.log(`Hello ${name}, you are ${age} years old.`);
