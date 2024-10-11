6b31278361776e6de6fc256b6d18adf98e0b4154
mmc7865 <mmc8791@gmail.com>
2024-10-11 15:24:15 +0530

    5th program Done:

---
+++ incremetDecrement.js
@@ -0,0 +1,9 @@
+let a=11;
+let b=22;
+let c;
+         
+        c = a + b + a++ + b++ + ++a + ++b;  //in this equation :- c = 11 + 22 + 11 + 22 + 13 + 24 = 103
+         
+        console.log("a="+a);
+        console.log("b="+b);
+        console.log("c="+c);
