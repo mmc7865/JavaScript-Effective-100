4b62ad7b6ddf91174804894d97d0c442b091fd88
mmc7865 <mmc8791@gmail.com>
2024-10-11 15:04:11 +0530

    secondProgram Done;

---
+++ printMsg.js
@@ -0,0 +1,8 @@
+// Print the messege along with values and sum
+// 	Ex - The sum of 45 & 12  = 57
+
+
+let num1 = 45;
+let num2 = 12;
+
+console.log(`Sum of ${num1} and ${num2} is = `, num1 + num2);
