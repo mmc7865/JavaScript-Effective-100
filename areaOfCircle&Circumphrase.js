d47903b7f2718dea0494a427f5e12af164a52ab3
mmc7865 <mmc8791@gmail.com>
2024-10-11 22:37:54 +0530

    11th Program Done:

---
+++ areaOfCircle&Circumphrase.js
@@ -0,0 +1,9 @@
+// Find circumference and area of circle
+
+let r = +prompt("Enter the radius");
+
+let circum = 2*Math.PI*r;
+let area = Math.PI*r*r;
+console.log("Circumference of circle is = "+circum.toFixed(2));
+console.log("Area of circle is = "+area.toFixed(2));
+
