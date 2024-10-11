d7e9bba38aca7941260c442a7e5b91f7429ec48d
mmc7865 <mmc8791@gmail.com>
2024-10-11 22:53:17 +0530

    13th Program Done:

---
+++ acceptGenderGreet.js
@@ -0,0 +1,10 @@
+// Accept the gender from the user as char and print the respective greeting message
+//       Ex - Good Morning Sir (on the basis of gender)
+
+let gender = prompt("Enter your Gender")
+if (gender == "male") {
+    console.log("Good Morning Sir");
+    
+} else {
+    console.log("Good Morning Mam");
+}
