5cc0bfa72ba5549af5dac2223d672c5d463cee76
mmc7865 <mmc8791@gmail.com>
2024-10-11 23:01:05 +0530

    14th Program Done:

---
+++ removeErrorGenderGreet.js
@@ -0,0 +1,13 @@
+// Extend the previous program and handle the wrong inputs.
+//       Print Good Morning sir for input m or M & Good morning Mam for input F or f 
+//       else print Wrong Input
+
+let gender = prompt("Enter the Gender")
+if (gender == "male" || gender == "Male") {
+    console.log("Good Morning Sir");
+} else if(gender == "female" || gender == "Female"){
+    console.log("Good Morning Mam");
+}else{
+    console.log("Wrong Imput");
+    
+}
