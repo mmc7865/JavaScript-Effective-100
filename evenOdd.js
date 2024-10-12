43d3028e59c85cecd03da61fbec1ae100b7e733a
mmc7865 <mmc8791@gmail.com>
2024-10-12 17:19:39 +0530

    15th Program Done:

---
+++ evenOdd.js
@@ -0,0 +1,9 @@
+// Accept an integer and check whether it is an even number or odd.
+
+let num = +prompt("Enter your number")
+if (num % 2 == 0) {
+    console.log("Your number is Even");
+} else {
+    console.log("Your numbers is Odd");
+    
+}
