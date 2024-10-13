7e86c0588cd8fd929acb32fe8c6c2b288c6424b0
mmc7865 <mmc8791@gmail.com>
2024-10-13 11:20:44 +0530

    26th Program Done:

---
+++ tablePrint.js
@@ -0,0 +1,11 @@
+// Take a number as input and print its table
+//        5 * 1 = 5
+//        5 * 2 = 10 ... up to 10 terms
+
+
+let num = +prompt('Enter your number')
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num} * ${i} = `,num * i);
+    
+    
+}
