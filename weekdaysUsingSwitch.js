5d3ba3f8e0b7aaa4788f536b4487a1c5f645ae7a
mmc7865 <mmc8791@gmail.com>
2024-10-15 17:31:55 +0530

    42 Program Done:

---
+++ weekdaysUsingSwitch.js
@@ -0,0 +1,30 @@
+// Print Weekday using Switch
+
+let number = +prompt('Enter your number')
+switch (number) {
+    case 1:
+        console.log("It's Monday");
+        break;
+    case 2:
+        console.log("It's Tuesday");
+        break;
+    case 3:
+        console.log("It's Wednesday");
+        break;
+    case 4:
+        console.log("It's Thursday");
+        break;
+    case 5:
+        console.log("It's Friday");
+        break;
+    case 6:
+        console.log("It's Saturday");
+        break;
+    case 7:
+        console.log("It's Sunday");
+        break;
+
+    default:
+        console.log("You entered invalid input please enter 1 to 7 only");
+        break;
+}
