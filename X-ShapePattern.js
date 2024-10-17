0080aa404e06d0c01985d9823bad93254d01e109
mmc7865 <mmc8791@gmail.com>
2024-10-17 16:09:06 +0530

    52 Program Done:

---
+++ X-ShapePattern.js
@@ -0,0 +1,16 @@
+
+
+let n = 5;
+let line;
+for (let i = 1; i <= 2*n ; i++) {
+    line = '';
+    for(let j = 1; j < 2*n; j++){
+       if (i == j || i + j == 2*n) {
+        line +="* "
+       } else {
+        line +="  " 
+       }
+    }
+ console.log(line);
+     
+}
