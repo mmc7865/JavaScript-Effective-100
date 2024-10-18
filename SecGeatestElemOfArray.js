e6fc51bcd004ce697df7a20e4be06ffddb6d7e1c
mmc7865 <mmc8791@gmail.com>
2024-10-18 15:49:51 +0530

    56 Program Done:

---
+++ SecGeatestElemOfArray.js
@@ -0,0 +1,15 @@
+// Find the second greatest element 
+//       {2, 96, 69, 77, 145, 20} = Second greatest element = 96
+
+let arr = [2, 96, 69, 107, 145, 20];
+let max = 0;
+let secMax = 0;
+arr.forEach((e, index) => {
+    if (e > arr[max]) {
+        secMax = max;
+        max = index;
+       
+    }
+    
+});
+console.log('Second greatest number is = '+ arr[secMax]);
