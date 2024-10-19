97357de49fafb219ac261d51091d3b54ceb44123
mmc7865 <mmc8791@gmail.com>
2024-10-19 16:47:03 +0530

    60 Program Done:

--- rotatioBy-K-Array.js
+++ rotatioBy-K-Array.js
@@ -4,8 +4,6 @@ let arr = [1,2,3,4,5,6,7,8,9]
 console.log(arr);
 let k = 8;
 console.log([...arr.slice(k,arr.length), ...arr.slice(0,k)]);
-console.log()
-
 // logic-2 if user input greater than array length than this logic work
 // let n = arr.length-1
 // k = k / n;
